b11ee7e4792accc6fe06dc19e1ab90c6de74a3f9
krysceejay <chrischimachijioke@gmail.com>
2021-03-25 15:31:43 +0100

    added pdf frame

---
+++ pages/resume.js
@@ -0,0 +1,11 @@
+
+const resume = () => {
+    return (
+        <div>
+            <iframe src="/chris-resume.pdf" width="100%" height="800px">
+            </iframe>
+        </div>
+    )
+}
+
+export default resume
